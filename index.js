576fc1b4faac7135593b9d5bcdf24ba63af5af7b
Riya moni <shammiriya455@gmail.com>
2023-07-19 10:50:06 -0700

    add pement Intent in stripe

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require("cors")
-require("dotenv").config()
-const port = process.env.PORT || 5000
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const stripe = require('stripe')(process.env.pement_key)
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
 
 
 
 // middleware
 app.use(cors());
 app.use(express.json());
-var jwt = require('jsonwebtoken');
+
 
 
 
@@ -58,6 +65,7 @@ async function run() {
         const courseCollection = client.db("creative_hero").collection("course");
         const selectCourseCollection = client.db("creative_hero").collection("select");
         const feedbackCollection = client.db("creative_hero").collection("feedback");
+        const paymentCollection = client.db("creative_hero").collection("pement");
 
 
 
@@ -82,6 +90,21 @@ async function run() {
             const result = await feedbackCollection.find().toArray();
             res.send(result);
         })
+
+        app.get("/enrollclass", async (req, res) => {
+            const { email } = req.query; // Assuming you pass the user's email as a query parameter
+          
+            try {
+              // Use your MongoDB database and collection to find user-specific data
+              const userEnrollments = await selectCourseCollection.find({ email }).toArray();
+          
+              // Send the user-specific data as the response
+              res.json(userEnrollments);
+            } catch (error) {
+              console.error("Error fetching user data:", error);
+              res.status(500).json({ error: "An error occurred while fetching user data." });
+            }
+          });
 
 
         const verifyAdmin = async (req, res, next) => {
@@ -130,10 +153,7 @@ async function run() {
         })
 
 
-        //     app.get('/instractor', (req, res) => {
-        //         const result = 
-        //   const instractor = await userCollection.find()
-        //     })
+       
 
 
         app.get('/user/instractor/:email', verifyJWT, async (req, res) => {
@@ -291,11 +311,6 @@ async function run() {
             console.log(item);
 
             try {
-                const existingSelection = await selectCourseCollection.findOne({ _id: item._id });
-                if (existingSelection) {
-                    res.status(400).json({ error: 'This class has already been selected.' });
-                    return;
-                }
 
                 const result = await selectCourseCollection.insertOne(item);
                 res.json(result);
@@ -324,11 +339,10 @@ async function run() {
         // deleteclass
 
         app.delete('/deleteCourse/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log(id);
-            const quiry = { _id: id };
-            const result = await selectCourseCollection.deleteOne(quiry);
-            console.log(result);
+            const id  = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await selectCourseCollection.deleteOne(query);
+           
             res.send(result);
         })
 
@@ -341,7 +355,7 @@ async function run() {
 
         app.patch("/allClass/Approve/:id", async (req, res) => {
             const id = req.params.id;
-            console.log(id);
+           
             const filter = { _id: new ObjectId(id) };
 
             const updateDoc = {
@@ -398,11 +412,50 @@ async function run() {
 
 
 
+// pement
+app.post("/create-payment-intent", async (req, res) => {
+    const { price } = req.body;
+    const amount = price * 100;
+ 
+    try {
+      // ... Your existing code ...
+  
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+  
+      // ... Your existing code ...
+  
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    } catch (error) {
+      console.error("Error creating PaymentIntent:", error);
+      res.status(500).json({ error: "An error occurred while creating the PaymentIntent." });
+    }
+  });
 
 
 
 
-        await client.db("admin").command({ ping: 1 });
+  
+
+  
+  
+
+  app.post('/payments',  async (req, res) => {
+    const payment = req.body;
+    
+    const insertResult = await paymentCollection.insertOne(payment);
+
+    const query = { _id: { $in: payment.cartItems.map(id => new ObjectId(id)) } }
+    const deleteResult = await selectCourseCollection.deleteMany(query)
+
+    res.send({ insertResult, deleteResult });
+  })
+
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
